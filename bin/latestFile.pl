f4802aa9856b831fd86a2df1a445b1e55b788e1a
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2014-09-23 15:00:25 -0700

    Script for notifying new data.

---
+++ bin/latestFile.pl
@@ -0,0 +1,58 @@
+#!/bin/perl
+use strict;
+
+my $maxAgeSec = 60000;
+my $dir  = "/home/alja/tmpm3/test";
+my $lastFile = "/tmp/Log/LastFile";
+
+sub readLineFromFile
+{
+# Returns the first line from file or "" if the file does not exist.
+# Dies if file can not be opened for reading.
+    my $filename = shift;
+    return "" unless -e $filename;
+    open F, $filename or die("Can't read_line_from_file!");
+    my $line = <F>;
+    close F;
+    chomp $line;
+    return $line;
+}
+
+while (1) {
+    my $ref    = "/tmp/cmsShow-tmp.txt";
+    system("touch -d \"-$maxAgeSec seconds\" $ref");
+    my $lc=`find $dir -maxdepth 1 -mindepth 1 -name \*.root -newer $ref `; 
+    my @candidates = split("\n",$lc);
+
+
+    my %hash;
+    foreach(@candidates) {
+      my $cnd = $_;
+      my $delta =  (stat($cnd))[9] - (stat($ref))[9];
+      if ($delta > 1) {
+	print("candidate $_ ", (stat($cnd))[9] , " ", $delta, "\n");
+	$hash{ $delta } = $cnd;
+      }
+    }
+
+    if (%hash) {
+      my @times = sort {$a<=>$b} keys %hash;
+      my $latestt =  @times[-1];
+      my $latest = $hash{$latestt};
+
+      # notify the latest file from the list if diferent from previous
+      my $sp = readLineFromFile("$lastFile");
+      # sort files by modification time
+
+      if ($sp ne $latest ) {
+        system("echo $hash{$latestt} > $lastFile");
+	print "LastFile = $latest\n";
+      }
+      else {
+	printf("No new file.\n");
+      }
+
+    }
+    # sleep 1 second before checking new file
+    sleep 1;
+}
