639c3df074ef0236c6611d1d2673eb0a7299ec10
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2015-03-26 14:48:28 -0700

    New script to monitor disk, cpu, and memory usage.

---
+++ bin/fw-sysstat.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+recmail=amraktadel@ucsd.edu
+host=`hostname`
+pid=`ps --no-headers -opid -C "cmsShow.exe"`
+
+
+############################################
+# MEMORY ###################################
+############################################
+export memLimit=3000000;
+memMSG=`pmap $pid |tail -1 |  perl -ne 'if (~/total\s+(\d+)K/){ $mem=$1; if ($mem > $ENV{memLimit}) {printf "memory usage exceed\n  ${mem}K > $ENV{memLimit}K\n"};  }'`
+if [ -n "$memMSG" ]
+then
+   echo $memMSG
+   echo $memMSG |  mail -s "Memory usage $host" "$recmail"
+fi
+exit
+############################################
+# CPU    ###################################
+############################################
+export cpuLimit=90;
+cpuMSG=`ps -p $pid -o %cpu | tail -1 | perl -e '{ $cpu=$_; chomp $cpu; if ($cpu > $ENV{cpuLimit}) {print "cpu usage exceed\n, $cpu > $ENV{cpuLimit} \n"} }'`
+if [ -n "$cpuMSG" ]
+then
+   echo $cpuMSG
+   echo $cpuMSG |  mail -s "CPU usage $host" "$recmail"
+fi
+
+
+############################################
+# DISK    ##################################
+############################################
+export diskLimit=60
+
+for i in /home /eventdisplay
+do
+data=$i
+diskMSG=`df $data | tail -1 |  perl -ne 'if (~/\s+(\d+)\%\s+(.+)$/){ $disk=$1; $loc=$2 ;if ($disk > $ENV{diskLimit}) {print "disku sage exceed\n $disk > $ENV{diskLimit}\n"};  }'`
+if [ -n "$diskMSG" ]
+then
+    echo $diskMSG
+    echo $diskMSG  | mail -s "disk usage $host $data" "$recmail"
+fi
+done
